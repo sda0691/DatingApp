cfdf887b732cda2c8e22e04e429e83c5160ac4ce
jpark home <jpark@cdsglobal.ca>
2019-11-10 22:40:46 -0500

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgForOf } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'DatingApp-SPA';
+}
